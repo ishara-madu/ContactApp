7aaf41b71ae4acb4db0f8190df242ea5d2697f64
Ishara <isharamadushankab@gmail.com>
2024-12-21 21:21:41 +0530

    first commit

---
+++ app/src/main/java/com/pixeleye/studentmanagementsystem/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.pixeleye.studentmanagementsystem
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var addButton:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        addButton = findViewById(R.id.addButton)
+
+        addButton.setOnClickListener{
+            val intent = Intent(this,InsertStudentActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
