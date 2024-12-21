6a975651753ea443161da47d3d8d4c98b1bebfde
Ishara <isharamadushankab@gmail.com>
2024-12-21 22:06:58 +0530

    updated activity_insert_student.xml

---
+++ app/src/main/java/com/pixeleye/studentmanagementsystem/DatabaseHelper.kt
@@ -0,0 +1,4 @@
+package com.pixeleye.studentmanagementsystem
+
+class DatabaseHelper {
+}
--- app/src/main/java/com/pixeleye/studentmanagementsystem/MainActivity.kt
+++ app/src/main/java/com/pixeleye/studentmanagementsystem/MainActivity.kt
@@ -4,13 +4,15 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var addButton:Button
+    private lateinit var addButton:FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
